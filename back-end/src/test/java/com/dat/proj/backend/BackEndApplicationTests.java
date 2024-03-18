03641167e2cb51ed8c9991b371b9ff959e3fdb2c
DataRaj <dhadas808@yahoo.com>
2024-03-18 23:53:36 +0530

    reintiated the back-end project

---
+++ back-end/src/test/java/com/dat/proj/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat.proj.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

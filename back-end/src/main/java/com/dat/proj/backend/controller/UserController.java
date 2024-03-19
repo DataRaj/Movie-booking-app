a6c88019b5a4d4cc0d6133bca62f34cc88cc8b41
DataRaj <dhadas808@yahoo.com>
2024-03-19 17:47:02 +0530

    added remaining repositories

--- back-end/src/main/java/com/dat/proj/backend/controller/UserController.java
+++ back-end/src/main/java/com/dat/proj/backend/controller/UserController.java
@@ -73,3 +73,117 @@ import java.util.regex.Pattern;
 
     }
 
+    /**
+     * SignUp of a user
+     * This API should not be authenticated
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addUser(@RequestBody User user) {
+        try {
+            this.LOGGER.info("addUser() called with input:  " + user.toString());
+            User newUser = user;
+            validateInput(newUser);
+            String userName = getUserName(user).replaceAll("\\s", "");
+            newUser.setUserName(userName);
+            newUser.setCreatedOn(LocalDateTime.now());
+            this.LOGGER.info("addUser() setting username as   " + newUser.getUserName());
+            this.userReposiitory.save(newUser);
+            this.LOGGER.info("successfully saved user object " + newUser.toString());
+            return new ResponseEntity<>(this.responseParser.build(HttpStatus.CREATED.value(), "Successfully saved user with user name: "+newUser.getUserName(),
+                    "Successfully saved user with user name: "+newUser.getUserName()), HttpStatus.CREATED);
+        } catch (IllegalArgumentException ex) {
+            this.LOGGER.error("Error Saving user object " + ex.getMessage());
+            return new ResponseEntity<>(
+                    this.responseParser.build(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), ex.getMessage()),
+                    HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            this.LOGGER.error("Error Saving user object " + e.getMessage());
+            e.printStackTrace();
+            return new ResponseEntity<>(
+                    this.responseParser.build(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
+
+    /**
+     *
+     * @param authRequest
+     * @return
+     */
+    @RequestMapping(value = "/token", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<Object> getToken(@RequestBody AuthRequest authRequest) {
+        JSONObject result = new JSONObject();
+        try {
+            this.LOGGER.info(
+                    "getToken() called with username as : " + authRequest.getUsername() + " and password as : *****");
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+            String token = jwtUtils.generateToken(authRequest.getUsername());
+            this.LOGGER.info("getToken() successfully got the jwt token " + token);
+            result.put("token", token);
+            result.put("status", "success");
+            this.LOGGER.info("getToken() returning token with status 200 ");
+            return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+        } catch (Exception e) {
+            this.LOGGER.info("getToken() Error creating jwt token :  " + e.getMessage());
+            result.put("message", e.getMessage() + " cause: " + e.getCause());
+            result.put("status", "failed");
+            e.printStackTrace();
+            return new ResponseEntity<>(result.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    /**
+     *
+     * @param user
+     */
+    private void validateInput(User user) {
+        try {
+            Assert.notNull(user, "User object must not be null");
+            Assert.hasLength(user.getFirstName(), "User first name must not be null or empty");
+            Assert.hasLength(user.getLastName(), "User last name must not be null or empty");
+            Assert.hasLength(user.getMobileNumber(), "User mobile number must not be empty");
+            Assert.hasLength(user.getUserType().toString(), "User type must not be null or empty");
+            Assert.hasLength(user.getEmail(), "Email must not be null or empty");
+            Assert.hasLength(user.getAuthentication(), "password must not be null or empty");
+            Assert.isTrue(user.getMobileNumber().length() == 10, "Invalid mobile number");
+            switch (user.getUserType().toString().toUpperCase()) {
+                case "ADMIN":
+                    user.setUserType(UserType.ADMIN);
+                    this.LOGGER.debug("validateInput(user), User type found as ADMIN");
+                    break;
+                case "NORMAL":
+                    user.setUserType(UserType.NORMAL);
+                    this.LOGGER.debug("validateInput(user), User type found as NORMAL");
+                    break;
+                default:
+                    Assert.isTrue(false, "Only values allowd for user type is : Admin or Normal");
+            }
+            Matcher matcher = VALIDATION_OF_EMAIL_ADDRESS_REG
+                    .matcher(user.getEmail());
+            Assert.isTrue(matcher.find(), "Invalid email id");
+            Pattern p = Pattern.compile("(0/91)?[7-9][0-9]{9}");
+            Matcher m = p.matcher(user.getMobileNumber());
+            Assert.isTrue((m.find() && m.group().equals(user.getMobileNumber())), "Invalid mobile number");
+        } catch (IllegalArgumentException e) {
+            this.LOGGER.error("input error ", e.getMessage());
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
+    }
+
+    /**
+     * @param user
+     * @return
+     */
+    private String getUserName(User user) {
+        return (user.getLastName() + user.getFirstName() + user.getMobileNumber());
+
+    }
+}
+

5c2e277bb650360fd385f342fc887881ef281e59
Dattaraj Dhadas <87413639+DataRaj@users.noreply.github.com>
2024-04-17 14:14:39 -0400

    Create AnimatedPage.tsx

---
+++ front-end/src/components/AnimatedPage.tsx
@@ -0,0 +1,21 @@
+import { motion } from 'framer-motion'
+
+const animations = {
+  initial: { opacity: 0, y: -100 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 100 }
+}
+
+export const AnimatedPage = ({ children }:{children : JSX.Element}) => {
+  return (
+    <motion.div
+      variants={animations}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 0.2 }}
+    >
+      {children}
+    </motion.div>
+  )
+}

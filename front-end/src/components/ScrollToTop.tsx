8cd8b72869000b26832414f5f418f5cc19134f16
Dattaraj Dhadas <87413639+DataRaj@users.noreply.github.com>
2024-04-17 14:16:48 -0400

    Add files via upload

---
+++ front-end/src/components/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+export const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    const body = document.querySelector('#root')
+    body?.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }, [pathname])
+
+  return null
+}
